ec71e83997cb05da382363bb62a0c270cb4f0644
AlbertSaulo <92164192+AlbertSaulo@users.noreply.github.com>
2021-11-17 16:37:42 +0100

    Add files via upload

---
+++ ecommerce/htdocs/js/funcions.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){ 
+ $("#graus").change(function(){ 
+ $.ajax({url: "mencions.php?grau=" + $("#graus").val(), success:  function(result){ 
+ $("#mencions").html(result); 
+ }}); 
+ }); 
+}); 
---
+++ ecommerce/htdocs/models/validarUsuario.php
@@ -0,0 +1,41 @@
+<?php
+
+function validaUsuario($connexio)
+{
+
+    $email = $_REQUEST['email'];
+    $contrasena =  $_REQUEST['contraseña'];
+
+
+    $sql = "SELECT email, contraseña FROM usuarios WHERE email=:email";  // AND contraseña=:contrasena";
+
+    $my_Insert_Statement = $connexio->prepare($sql);
+
+
+    $my_Insert_Statement->bindParam("email", $email,PDO::PARAM_STR);
+    $my_Insert_Statement->execute();
+
+    $row = $my_Insert_Statement->fetch();
+    $filas = $my_Insert_Statement->rowCount();
+
+    if ($filas == 1)
+    {
+        if (password_verify($contrasena, $row['contraseña']))
+        {
+            //echo 'Sesion iniciada';
+            return 0;
+        }
+        else{
+            //echo 'Contraseña incorrecta';
+            return 1;
+        }
+
+    }
+    else
+    {
+        //echo 'Email incorrecto';
+        return 2;
+    }
+
+
+}
